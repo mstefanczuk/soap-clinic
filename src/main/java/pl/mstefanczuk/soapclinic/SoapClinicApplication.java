c58a1ba7c9894c9383cf7c34db2e208a1627255f
Michał Stefańczuk <michal.stefanczuk@gmail.com>
2019-05-06 14:38:38 +0200

    Initial commit

---
+++ src/main/java/pl/mstefanczuk/soapclinic/SoapClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.mstefanczuk.soapclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mstefanczuk/soapclinic/SoapClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.mstefanczuk.soapclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoapClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
